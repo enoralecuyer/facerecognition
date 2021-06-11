363a1b93576caa6f7cc3d10a7a451cb777bef41c
enoralecuyer <48727972+enoralecuyer@users.noreply.github.com>
2021-06-11 10:29:43 -0700

    changed nav

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
     <nav className="dt w-100 border-box pa3 ph5-ns">
       <p className="f3 link dim pink pa3 pointer dib">Face Recognition</p>
       <div className="dtc v-mid w-75 tr toggle-container">
-        <p className="f3 link dim pink pa3 pointer dib"><a href="http://www.enoralecuyer.com/">Contact</a></p>
+        <p className="f3 link dim pink pa3 pointer dib"><a href="http://www.enoralecuyer.com/">About</a></p>
       </div>
     </nav>
   );
